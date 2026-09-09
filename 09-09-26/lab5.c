44ba3d72b3bf502b8a3756a531867e462a575536
Vangara Keerthana <your_email@example.com>
2026-09-09 16:32:54 +0530

    Add full code to lab5.c

--- 09-09-26/lab5.c
+++ 09-09-26/lab5.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main() {
+    char str[100];
+    int len = 0;
+    int isPalindrome = 1;
+
+    scanf(" %[^\n]s", str);
+
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    for (int i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - 1 - i]) {
+            isPalindrome = 0;
+            break;
+        }
+    }
+
+    if (isPalindrome) {
+        printf("Palindrome\n");
+    } else {
+        printf("Not a Palindrome\n");
+    }
+
+    return 0;
+}
